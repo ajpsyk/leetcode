a9c0fb683f8a8b77cf5c51beae7255468754ceff
ajpsyk <ajpsyk@gmail.com>
2024-11-04 18:26:28 -0800

    reverse a linked list solution

--- Reverse a Linked List.py
+++ Reverse a Linked List.py
@@ -28,7 +28,33 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return ListNode()
+        # head is None case
+        if head is None:
+            return head
+
+        curr_node = head
+        stack = []
+
+        # traverse over list adding each element to stack
+        # stop once last element is reached (its next element is None)
+        while curr_node.next is not None:
+            stack.append(curr_node)
+            curr_node = curr_node.next
+
+
+        # store current node as new head
+        new_head = curr_node
+
+        while len(stack) > 0:
+            # pop from stack until empty
+            # make current node's next the popped value
+            curr_node.next = stack.pop()
+
+            # advance current node to next
+            curr_node = curr_node.next
+
+        curr_node.next = None
+        return new_head
 
 
 solution = Solution()
@@ -43,5 +69,19 @@ node3.next = node4
 head2 = None
 
 
-print(Solution.reverseList(head1))
-print(Solution.reverseList(head2))
+ll = solution.reverseList(head1)
+while ll:
+    print(ll.val)
+    ll = ll.next
+print(solution.reverseList(head2))
+
+
+
+
+
+
+
+
+
+
+
