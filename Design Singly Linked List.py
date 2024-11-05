505595846b2954a3eba4a3b5ac882e4bccbb9429
ajpsyk <ajpsyk@gmail.com>
2024-11-04 16:46:45 -0800

    design singly linked list solution

--- Design Singly Linked List.py
+++ Design Singly Linked List.py
@@ -30,25 +30,89 @@ The index int i provided to get(int i) and remove(int i) is guaranteed to be gre
 
 from typing import List
 
+
 class LinkedList:
 
     def __init__(self):
-        pass
+        self.head = None
 
     def get(self, index: int) -> int:
-        pass
+        curr_node = self.head
+        curr_index = 0
+
+        while curr_node is not None:
+            if curr_index == index:
+                return curr_node.val
+            curr_index += 1
+            curr_node = curr_node.next
+
+        return -1
+
+
+
 
     def insertHead(self, val: int) -> None:
-        pass
+        if self.head is None:
+            self.head = ListNode(val)
+        else:
+            new_node = ListNode(val)
+            new_node.next = self.head
+            self.head = new_node
 
     def insertTail(self, val: int) -> None:
-        pass
+        if self.head is None:
+            self.head = ListNode(val)
+        else:
+            curr_node = self.head
+
+            while curr_node.next is not None:
+                curr_node = curr_node.next
+
+            curr_node.next = ListNode(val)
 
     def remove(self, index: int) -> bool:
-        pass
+        # list is empty
+        if self.head is None:
+            return False
+        # index to remove is head
+        if index == 0:
+            self.head = self.head.next
+            return True
+
+        # index to remove is not head and is a valid index
+        # index to remove is out of bounds
+        curr_node = self.head
+        next_index = 1
+
+        while curr_node.next is not None:
+            if next_index == index:
+                curr_node.next = curr_node.next.next
+                return True
+            next_index += 1
+            curr_node = curr_node.next
+
+        return False
 
     def getValues(self) -> List[int]:
-        pass
+        values = []
+        curr_node = self.head
+
+        while curr_node is not None:
+            values.append(curr_node.val)
+            curr_node = curr_node.next
+
+        return values
+
+class ListNode:
+
+    def __init__(self, val: int):
+        self.val = val
+        self.next = None
+
+ll = LinkedList()
+ll.insertTail(1)
+ll.insertTail(2)
+print(ll.get(1))
 
 ll = LinkedList()
 ll.insertHead(1)
