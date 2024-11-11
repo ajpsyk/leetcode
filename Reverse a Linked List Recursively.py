9931fe71d5dc8748124d9f12a0dc200be04a84d2
ajpsyk <ajpsyk@gmail.com>
2024-11-10 16:30:46 -0800

    reverse linked list recursive solution

--- Reverse a Linked List Recursively.py
+++ Reverse a Linked List Recursively.py
@@ -17,12 +17,24 @@ Constraints:
 -1000 <= Node.val <= 1000
 
 """
-
+from typing import Optional
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None:
+            return head
+
+        if head.next is None:
+            return head
+
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
+
+
