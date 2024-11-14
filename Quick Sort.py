2e22075f046c30bdcee380faaf66f8023e2344cb
ajpsyk <ajpsyk@gmail.com>
2024-11-13 20:43:05 -0800

    quick sort solution

--- Quick Sort.py
+++ Quick Sort.py
@@ -41,3 +41,25 @@ Output:
 #         self.value = value
 class Solution:
     def quickSort(self, pairs: List[Pair]) -> List[Pair]:
+        self.quickSortHelper(pairs, 0, len(pairs) - 1)
+        return pairs
+
+    def quickSortHelper(self, pairs: List[Pair], s, e) -> None:
+        if e - s + 1 <= 1:
+            return
+
+        pivot = pairs[e]
+        left = s
+
+        for i in range(s, e):
+            if pairs[i].key < pivot.key:
+                temp = pairs[left]
+                pairs[left] = pairs[i]
+                pairs[i] = temp
+                left += 1
+
+        pairs[e] = pairs[left]
+        pairs[left] = pivot
+
+        self.quickSortHelper(pairs, s, left - 1)
+        self.quickSortHelper(pairs, left + 1, e)
